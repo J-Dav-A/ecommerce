3d2c8746c54d6f85fe69c35ea6f7f2a878ab4575
Erwin Harder <erwinj.harderg@uqvirtual.edu.co>
2026-09-18 22:17:13 -0500

    crear caso de uso DejarComentario

---
+++ src/main/java/com/uniquindio/ecommerce/application/usecase/DejarComentario.java
@@ -0,0 +1,15 @@
+package com.uniquindio.ecommerce.application.usecase;
+
+public class DejarComentario {
+
+    /**
+     * Caso de uso: DejarComentario
+     *
+     * Permite a un comprador dejar un comentario
+     * y calificar un producto adquirido.
+     *
+     * Regla de negocio:
+     * Un comprador no puede calificar un producto
+     * sin haberlo comprado.
+     */
+}

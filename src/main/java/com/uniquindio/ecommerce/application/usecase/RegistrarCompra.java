db145451be4027ac0bb096a27a4e2ef9d4291151
J-Dav-A <159507313+J-Dav-A@users.noreply.github.com>
2026-09-20 01:42:36 -0500

    creacion de caso de uso RegistrarCompra

---
+++ src/main/java/com/uniquindio/ecommerce/application/usecase/RegistrarCompra.java
@@ -0,0 +1,15 @@
+package com.uniquindio.ecommerce.application.usecase;
+
+public class RegistrarCompra {
+    /**
+     * Caso de uso: RegistrarCompra
+     *
+     * Permite a un comprador adquirir un videojuego o contenido
+     * digital publicado en el marketplace mediante una Oferta.
+     *
+     * Regla de negocio:
+     * Una clave digital solo puede ser asignada a un comprador
+     * una vez y no puede volver a comercializarse después de
+     * haber sido utilizada o asignada.
+     */
+}
---
+++ src/main/java/com/uniquindio/ecommerce/application/usecase/SolicitarReembolso.java
@@ -0,0 +1,4 @@
+package com.uniquindio.ecommerce.application.usecase;
+
+public class SolicitarReembolso {
+}
